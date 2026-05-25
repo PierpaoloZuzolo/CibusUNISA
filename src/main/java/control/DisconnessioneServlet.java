1ca6a018334e636702c66bfff001cd16033ce7f3
pierp <pierpaolozuzolo@gmail.com>
2026-05-25 21:56:32 +0200

    implementazione disconnessione

---
+++ src/main/java/control/DisconnessioneServlet.java
@@ -0,0 +1,32 @@
+package control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/logout")
+public class DisconnessioneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public DisconnessioneServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession(false) != null) {
+            request.getSession().invalidate(); 
+        }
+        
+        response.sendRedirect("home");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+
+}
