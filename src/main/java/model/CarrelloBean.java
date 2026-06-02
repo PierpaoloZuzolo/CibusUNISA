da12b4efbc89c78cf5473c01717c622f67aeb3f7
pierp <pierpaolozuzolo@gmail.com>
2026-06-02 12:20:41 +0200

    implementazione CarrelloBean.java

---
+++ src/main/java/model/CarrelloBean.java
@@ -0,0 +1,16 @@
+package model;
+
+public class CarrelloBean {
+	private ProdottoBean prodotto;
+    private int quantita;
+
+    public CarrelloBean(ProdottoBean prodotto, int quantita) {
+        this.prodotto = prodotto;
+        this.quantita = quantita;
+    }
+
+    public ProdottoBean getProdotto() { return prodotto; }
+    public void setProdotto(ProdottoBean prodotto) { this.prodotto = prodotto; }
+    public int getQuantita() { return quantita; }
+    public void setQuantita(int quantita) { this.quantita = quantita; }
+}
