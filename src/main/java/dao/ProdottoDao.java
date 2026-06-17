04a9b8fa311bf2975c8e4e234318c3b0a3825bbe
pierpaolozuzolo <pierpaolozuzolo@192.168.1.21>
2026-06-17 17:57:44 +0200

    modifica ProdottoDao.java

--- src/main/java/dao/ProdottoDao.java
+++ src/main/java/dao/ProdottoDao.java
@@ -1,17 +1,22 @@
 package dao;
 
 import model.ProdottoBean;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 public interface ProdottoDao {
 	
 	public void doSave(ProdottoBean u) throws SQLException;
-
-	public ProdottoBean doRetrieveByCodice(int codice) throws SQLException;
+	
+	public boolean doUpdate(ProdottoBean p) throws SQLException;
 	
 	public boolean doDelete(int codice) throws SQLException;
 	
 	public List<ProdottoBean> doRetrieveAllActive() throws SQLException;
+	
+	public List<ProdottoBean> doRetrieveAll() throws SQLException;
+	
 	
 }
